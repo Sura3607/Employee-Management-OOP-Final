44ba8f9278cedb28b36fea56d11d0d0ed31686e5
Sura <nguyenminhnhut3607@gmail.com>
2024-10-06 01:51:09 +0700

    Change HRManagement to ManagementLogic, add winform, change output ManagementLogic to classLibary

---
+++ HRManagement/ManagementLogic/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementLogic
+{
+    internal class Database
+    {
+        static public void LoadData()
+        {
+
+        }
+        public void SaveData()
+        {
+
+        }
+    }
+}
---
+++ HRManagement/Managenment_Windows/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ManagementLogic;
+
+namespace Managenment_Windows
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Employee a = new Employee("a", "123", "@123", "Abc", true, default, default, new HR(), default(int), "");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
